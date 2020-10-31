5c920bcfbc8d7c090fdf005b36b7c89400ca7fa9
Egor Kokhan <zhora.kokhan56@gmail.com>
2020-10-31 20:41:22 +0300

    #5 Add print information about all cats

--- src/main/java/com/netcracker/cats/controller/ConsoleController.java
+++ src/main/java/com/netcracker/cats/controller/ConsoleController.java
@@ -1,9 +1,61 @@
 package com.netcracker.cats.controller;
 
+import com.netcracker.cats.service.CatService;
+import com.netcracker.cats.service.CatServiceImpl;
+import com.netcracker.cats.util.ConsolePrinter;
+
+import java.util.Scanner;
+
 public class ConsoleController {
 
-    public void start() {
+    private final CatService catService = new CatServiceImpl();
+    private final ConsolePrinter consolePrinter = new ConsolePrinter();
 
+    public void printMenuItem(){
+        System.out.print("Select an action:" +
+                "\n1 - Print info about all cats." +
+                "\n2 - Add new cat." +
+                "\n3 - Search a cat by id." +
+                "\n4 - Exit." +
+                "\nSelect: "
+        );
+    }
+
+    public void start() {
+        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            printMenuItem();
+            while (!scanner.hasNextInt()) {
+                System.out.print("Incorrect input! Try again: ");
+                scanner.next();
+            }
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1: {
+                    consolePrinter.printAllCats(
+                            catService.getAll()
+                    );
+                    break;
+                }
+                case 2: {
+                    //TODO add catService.add method
+                    break;
+                }
+                case 3: {
+                    //TODO print info by ID
+                    break;
+                }
+                case 4: {
+                    break;          /*exit point*/
+                }
+                default: {
+                    System.out.println("Wrong choice");
+                    break;
+                }
+            }
+        } while (choice != 4);
     }
 
 }
--- src/main/java/com/netcracker/cats/util/ConsolePrinter.java
+++ src/main/java/com/netcracker/cats/util/ConsolePrinter.java
@@ -1,4 +1,21 @@
 package com.netcracker.cats.util;
 
+import com.netcracker.cats.model.Cat;
+
+import java.util.List;
+
 public class ConsolePrinter {
+
+    public void printAllCats(List<Cat> cats){
+        for (Cat cat : cats) {
+            printCatInfo(cat);
+        }
+    }
+
+    public void printCatInfo(Cat cat){
+        String catInfo = "Cat #" + cat.getId() + ". " +
+                "Name " + cat.getName() + ".";
+        System.out.println(catInfo);
+    }
+
 }
